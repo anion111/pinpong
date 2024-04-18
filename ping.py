5d2393a7cd23953cb995cdc984a6c834eb21cea8
anion111 <161601680+anion111@users.noreply.github.com>
2024-04-18 20:02:16 +0500

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,10 +1,10 @@
-from pygame import * 
-import time
-pygame.init()
+from pygame import* 
+
+from random import*
 
 
-win = pygame.display.set_mode((500,500))
-clck = pygame.time.Clock()
+win = display.set_mode((500,500))
+clck = time.Clock()
 game = True
 
 
@@ -30,60 +30,98 @@ class GameSprite(sprite.Sprite):
     #метод, отрисовывающий героя на окне
 
     def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+colors = [(245, 171, 234), (228, 174, 242), (98, 137, 245), (168, 225, 240) ,(134, 227, 193) ,(134, 227, 193)]
+
+
 
 font.init()
 font1 = font.Font(None, 80)
-winn = font1.render("YYYEEEPIIII, YAPI<3!! ,YEPI,YEPI!!!! >-<", True ,(()))
-lose = font1.rander('Ohh nooo!!!', True, (()))
+# winn = font1.render(, True , random.choice(colors))
+# lose = font1.rander('Ohh nooo!!!', True, random.choice(colors))
 
 
-mixer.init()
-mixser.music.loud("")
-mixser.music.play()
-
-sound1 = pg.mixer.Sound('')
-sound2 = pg.mixer.Sound('')
+text = "YYYEEEPIIII, YAPI<3!! ,YEPI,YEPI!!!! >-<"
 
 
-  class Player(GameSprite):
-     def update(self):
+
+
+# mixer.init()
+# mixer.music.loud("")
+# mixer.music.play()
+
+# sound1 = pg.mixer.Sound('')
+# sound2 = pg.mixer.Sound('')
+
+
+class Player(GameSprite):
+    def update(self):
         keys = key.get_pressed()
-        if keys[K_GREATER]
+        if keys[K_k]  :
+            self.rect.y += self.speed
+        if keys[K_l]:
+            self.rect.y -= self.speed
 
-        if keys[K_L]
+    def update1(self):
+        keys = key.get_pressed()
+        if keys[K_z] :
+            self.rect.y -= self.speed
+        if keys[K_s] :
+            self.rect.y += self.speed
 
-        if keys[K_z]
 
-        if keys[K_s]
 
-rar = GameSprite('hfr ktdsq.png', 30, 300, 20, 30, 20)
-ral = GameSprite( 'ракетка.png', 30, 300, 20, 30, 20)
-ball = GameSprite('rjn.png', 30, 300, 20, 30, 10)
 
+
+
+rar = Player('hfrktdsq.png', 450, 300, 20, 30, 20)
+ral = Player( 'ракетка.png', 30, 300, 80, 30, 20)
+ball = GameSprite("r.png", 90, 200, 60, 30, 10)
+finish = False
 live = 3
-       
+speedX = 3
+speedY = 3     
 while game:
 
-    if not srite.collide_rect( rar, ball):
-        live -= 1
-        if live == 0:    
-            sound1.play()
-            display.update()
-            lose.blit()
-            time.delay(2000)
-            break
+
     
+    if finish != True:
+        ball.rect.x += speedX
+        ball.rect.y += speedY
+
+        if ball.rect.y >= 450-50  or ball.rect.y <= 0:
+            speedY *= -1
 
 
 
-    for e in pygame.event.get():
+        if sprite.collide_rect(rar, ball) or sprite.collide_rect(ral, ball):
+            speedX *= -1
+
+
+        # if not sprite.collide_rect( rar, ball) or sprite.collide_rect(ral, ball):
+        #     live -= 1
+        #     if live == 0:    
+        #         # sound1.play()
+
+        #         # lose.blit()
+        #         time.delay(2000)
+        #         break
+
+    for e in event.get():
         if e.type == QUIT:
             game = False
-    
-    rar.update()      
 
-    win.fill(225,0,55)
+    for i in range(len(text)-1):
+        let = font1.render(text[i], True, (colors[1]))
+
+    win.blit(let, (200, 200))
+    win.fill((225,0,55))
+    ball.reset()
+    rar.update()      
+    rar.reset()
+    ral.update1()
+    ral.reset()
     clck.tick(60)
-    win.blit()
+    # win.blit()
     display.update()
